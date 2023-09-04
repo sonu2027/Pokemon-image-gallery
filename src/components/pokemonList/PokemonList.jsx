9f67beff49bfcaf2bbd44833e3fa81f56cf15e4f
Sonu Mondal <takeinfo2u@gmail.com>
2023-09-04 21:04:16 +0530

    Basic setup done

--- src/components/pokemonList/PokemonList.jsx
+++ src/components/pokemonList/PokemonList.jsx
@@ -5,22 +5,23 @@ import Pokemon from "../pokemon/Pokemon";
 function PokemonList() {
     const [pokemonList, setPokemonList] = useState([])
     const [isLoading, setIsLoading] = useState(true)
+    const POKEDEX_URL="https://pokeapi.co/api/v2/pokemon/"
     async function downloadPokemons() {
-        const response = await axios.get("https://pokeapi.co/api/v2/pokemon/")
+        const response = await axios.get(POKEDEX_URL) // This download list of 20 pokemon
         console.log("Response is:",response);
-        const pokemonResults = response.data.results
+        const pokemonResults = response.data.results // We get the array of pokemon from result
         console.log("Pokemon result:",pokemonResults);
         const pokemonResultPromise = pokemonResults.map((pokemon) =>
             axios.get(pokemon.url))
         console.log("Promise:",pokemonResultPromise);
         const pokemonData = await axios.all(pokemonResultPromise)
         console.log(pokemonData);
-        const res = pokemonData.map((pokeData) => {
+        const pokeListResult = pokemonData.map((pokeData) => {
             const pokemon = pokeData.data
             return { id: pokemon.id, name: pokemon.name, image: (pokemon.sprites.other) ? pokemon.sprites.other.dream_world.front_default : pokemon.sprites.front_shiny, types: pokemon.types }
         })
-        setPokemonList(res)
-        console.log(res);
+        setPokemonList(pokeListResult)
+        console.log(pokeListResult);
         setIsLoading(false)
     }
     useEffect(() => {
